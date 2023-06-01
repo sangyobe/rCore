cf1036d6f33bb496a2898015adbb8882825a72e9
Sangyup Yi <sean.yi@hyundai.com>
2023-06-01 15:04:33 +0900

    ake up test code

--- test_rcore.cpp
+++ test_rcore.cpp
@@ -1,19 +1,33 @@
 #include "rCore/rxInterpolator.h"
+#include <chrono>
 #include <iostream>
 
-int main() {
-  rCore::Interpolator::TYPE type = rCore::Interpolator::TYPE::LINEAR;
+void TestInterpolator(rCore::Interpolator::TYPE type, int sps, int loop) {
   double t0 = 0.0;
   double tf = 10.0;
-  rMath::VectorXd vi;
-  vi.resize(2);
-  vi << 0.0, 0.0;
-  rMath::VectorXd vf;
-  vf.resize(2);
-  vf << 10.0, 100.0;
-  rlab::rxcontrolsdk::rxInterpolator linIntp(type, t0, tf, vi, vf);
-  double p, v, a;
-  linIntp.interpolate(5.0, p, v, a);
-  std::cout << p << std::endl;
+  rMath::Vector3d vi;
+  vi << 0.0, 0.0, 0.0;
+  rMath::Vector3d vf;
+  vf << 5.0, 0.0, 0.0;
+  for (int j = 0; j < loop; j++) {
+    std::chrono::system_clock::time_point start =
+        std::chrono::system_clock::now();
+    for (int i = 0; i < sps; i++) {
+      rlab::rxcontrolsdk::rxInterpolator linIntp(type, t0, tf, vi, vf);
+      double p, v, a;
+      double t = t0 + (tf - t0) / sps * i;
+      linIntp.interpolate(t, p, v, a);
+      // std::cout << "(" << t << ", " << p << ")" << std::endl;
+    }
+    std::chrono::system_clock::time_point end =
+        std::chrono::system_clock::now();
+    std::chrono::duration<double> sec_elapsed = end - start;
+    std::cout << "elapsed time(" << j << " th)=" << sec_elapsed.count()
+              << " (sec)" << std::endl;
+  }
+}
+
+int main() {
+  TestInterpolator(rCore::Interpolator::TYPE::CUBIC, 1000000, 10);
   return 0;
 }
